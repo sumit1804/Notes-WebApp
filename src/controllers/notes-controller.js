92e2f2f953ba20efae992df668036c5b9acbe7f5
sumit1804 <sumit2002yadav2002@gmail.com>
2024-01-04 19:32:51 +0530

    major commit

--- src/controllers/notes-controller.js
+++ src/controllers/notes-controller.js
@@ -1,6 +1,7 @@
 import Note from "../models/Note.js";
 
 
+// function to get all the notes from database
 export const getAllNotes = async (req,res,next) => {
     try{
         const allNotes = await Note.find();
@@ -20,15 +21,48 @@ export const getAllNotes = async (req,res,next) => {
     }
 }
 
-export const createNotes = async (req,res,next) => {
+// function to get note by id
+export const getNoteById = async(req,res) => {
+    try {
+        const id = req.params.id;
+        const note = await Note.findById(id);
+        if(!note){
+            return res.status(404).json({
+                message:"note not found"
+            })
+        }
+        return res.status(200).json({
+            note:note,
+            message:"note found successfully"
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message:"internal server error..."
+        })
+    }
+}
+
+// function to create new notes in database
+export const createNote = async (req,res,next) => {
         try{
             const { title, content} = req.body;
+            if(title.length < 8){
+                return res.status(400).json({
+                    message:"title length too small"
+                })
+            }
+            if(content.length < 20){
+                return res.status(400).json({
+                    message:"content is very less"
+                })
+            }
             const note = new Note({
                 title:title,
                 content:content,
                 createdAt:new Date()
             })
-            note.save();
+            await note.save();
             return res.status(200).json({
                 note:note,
                 message:"note created successfully"
@@ -36,7 +70,54 @@ export const createNotes = async (req,res,next) => {
         }catch(err){
             console.log(err);
             return res.status(500).json({
-                message:"internal server error"
+                message:"internal server error..."
             })
         }
     }
+
+export const updateNote = async(req,res)=>{
+    try {
+        const {content} = req.body;
+        const noteId = req.params.id;
+        const note = await Note.findByIdAndUpdate(noteId,{
+            content:content,
+            updatedAt:new Date()
+        })
+        if(!note){
+            return res.status(404).json({
+                message:"Unable to update note"
+            })
+        }
+        const updatedNote = await Note.findById(noteId);
+        return res.status(200).json({
+            note:updatedNote,
+            message:"note updated successfully"
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message:"internal server error..."
+        })
+    }
+}
+
+export const deleteNote = async(req,res)=>{
+    try {
+        const id = req.params.id;
+        const note = await Note.findByIdAndDelete(id);
+        if(!note){
+            return res.status(404).json({
+                message:"unable to delete note"
+            })
+        }
+        return res.status(200).json({
+            note:note,
+            message:"note deleted successfully"
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message:"internal server error..."
+        })
+    }
+}
--- src/routes/notes-routes.js
+++ src/routes/notes-routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createNotes, getAllNotes } from "../controllers/notes-controller.js";
+import { createNote, deleteNote, getAllNotes, getNoteById, updateNote } from "../controllers/notes-controller.js";
 
 const router = Router();
 
 
 /* This route is to get all notes */
 router.get("/", getAllNotes);
+
+/* 
+This route is to get note by id 
+*/
+router.get("/:id", getNoteById)
 
 /*
 This route is for adding new notes
@@ -14,6 +19,20 @@ This route is for adding new notes
         "content":"content"
     }
  */
-router.post("/addNotes",createNotes);
+router.post("/addNote",createNote);
+
+/* 
+This route is for updating existed note
+    {
+        "title":"title",
+        "content":"content"
+    }
+ */
+router.put("/updateNote/:id", updateNote)
+
+/*
+This route is for deleting existed note
+ */
+router.delete("/delete/:id",deleteNote)
 
 export default router;
